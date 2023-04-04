973548a8fecc4c8fd409b817c3e58cd533d454a1
Isaac Harris-Holt <isaac@harris-holt.com>
2023-04-04 20:33:56 +0100

    add code for shutil deep dive

---
+++ 009-shutil-deep-dive/file_ops.py
@@ -0,0 +1,62 @@
+import shutil
+import re
+from pathlib import Path
+
+DIGIT_PATTERN = re.compile(r"(\d+)")
+
+
+def increment_numbers_in_string(string: str) -> str:
+    """Increment any numbers found in the given string."""
+    return DIGIT_PATTERN.sub(lambda m: str(int(m.group(1)) + 1), string)
+
+
+def increment_numbers_in_file_names(dir_path: Path):
+    """Increment any numbers found in file names in the given directory."""
+    for path in dir_path.iterdir():
+        if path.is_file():
+            new_name = increment_numbers_in_string(path.name)
+            if new_name != path.name:
+                shutil.move(path, path.with_name(new_name))
+
+
+def delete_odd_numbered_directories(dir_path: Path):
+    """Delete any directories with odd numbered names in the given directory."""
+    for path in dir_path.iterdir():
+        if path.is_dir():
+            if int(path.name) % 2 == 1:
+                shutil.rmtree(path)
+
+
+def non_destructive_copytree(src: Path, dst: Path):
+    """Copy the contents of the given source directory to the given destination
+    directory."""
+    for path in src.iterdir():
+        if path.is_file():
+            shutil.copy2(path, dst)
+        elif path.is_dir():
+            new_dir = dst / path.name
+            new_dir.mkdir()
+            non_destructive_copytree(path, new_dir)
+
+
+def copy_even_numbered_trees(src: Path, dst: Path):
+    def is_odd(_, files: list[str]) -> list[str]:
+        return [
+            file for file in files
+            if DIGIT_PATTERN.search(file)
+            and int(DIGIT_PATTERN.search(file).group(1)) % 2 == 1
+        ]
+
+    shutil.copytree(src, dst, dirs_exist_ok=True, ignore=is_odd)
+
+
+def clear_terminal():
+    """Fill the terminal with text."""
+    size = shutil.get_terminal_size()
+
+    for i in range(size.lines):
+        print(" " * size.columns)
+
+
+if __name__ == '__main__':
+    clear_terminal()
---
+++ 009-shutil-deep-dive/test_with_shutil.py
@@ -0,0 +1,63 @@
+import shutil
+import tempfile
+from pathlib import Path
+
+from file_ops import (
+    increment_numbers_in_file_names,
+    delete_odd_numbered_directories,
+    non_destructive_copytree,
+)
+
+
+def test_increment_numbers_in_file_names():
+    with tempfile.TemporaryDirectory() as tempdir:
+        shutil.copytree("my-test-dir", tempdir, dirs_exist_ok=True)
+        increment_numbers_in_file_names(Path(tempdir))
+
+        # Test files are present with new names
+        assert (Path(tempdir) / "2-foo.txt").exists()
+        assert (Path(tempdir) / "3-bar.txt").exists()
+
+        # Test files are not present with old names
+        assert not (Path(tempdir) / "1-foo.txt").exists()
+        assert not (Path(tempdir) / "2-bar.txt").exists()
+
+
+def test_delete_odd_numbered_directories():
+    with tempfile.TemporaryDirectory() as tempdir:
+        shutil.copytree("my-test-dir", tempdir, dirs_exist_ok=True)
+        delete_odd_numbered_directories(Path(tempdir))
+
+        # Test directories are present with new names
+        assert (Path(tempdir) / "2").exists()
+        assert (Path(tempdir) / "4").exists()
+
+        # Test directories are not present with old names
+        assert not (Path(tempdir) / "1").exists()
+        assert not (Path(tempdir) / "3").exists()
+
+
+def test_non_destructive_copytree():
+    with tempfile.TemporaryDirectory() as tempdir:
+        (Path(tempdir) / "really-important-file.txt").touch()
+
+        non_destructive_copytree(Path("my-test-dir"), Path(tempdir))
+
+        # Test files are present in new directory
+        assert (Path(tempdir) / "1-foo.txt").exists()
+        assert (Path(tempdir) / "2-bar.txt").exists()
+
+        # Test directories are present in new directory
+        assert (Path(tempdir) / "1").exists()
+        assert (Path(tempdir) / "2").exists()
+        assert (Path(tempdir) / "3").exists()
+        assert (Path(tempdir) / "4").exists()
+
+        # Test files in subdirectories are present in new directory
+        assert (Path(tempdir) / "1" / "1.txt").exists()
+        assert (Path(tempdir) / "2" / "2.txt").exists()
+        assert (Path(tempdir) / "3" / "3.txt").exists()
+        assert (Path(tempdir) / "4" / "4.txt").exists()
+
+        # Existing files are not overwritten
+        assert (Path(tempdir) / "really-important-file.txt").exists()
