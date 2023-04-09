dc912822191ddd3f12937ad537e2cb76d757dafe
Isaac Harris-Holt <isaac@harris-holt.com>
2023-04-09 13:46:08 +0100

    rename var to n

--- 010-creating-python-packages-with-rust/fib.py
+++ 010-creating-python-packages-with-rust/fib.py
@@ -14,15 +14,15 @@ def fibonacci(n: int) -> int:
 
 
 def main():
-    num = int(sys.argv[1])
-    print(f"{fibonacci(num) = }")
-    print(f"{fibbers.fib(num) = }")
+    n = int(sys.argv[1])
+    print(f"{fibonacci(n) = }")
+    print(f"{fibbers.fib(n) = }")
 
-    python_time_per_call = timeit(lambda: fibonacci(num), number=RUNS) / RUNS
+    python_time_per_call = timeit(lambda: fibonacci(n), number=RUNS) / RUNS
     print(f"\nPython μs per call: {python_time_per_call * 1_000_000:.2f} μs")
     print(f"Python ms per call: {python_time_per_call * 1_000:.2f} ms")
 
-    rust_time_per_call = timeit(lambda: fibbers.fib(num), number=RUNS) / RUNS
+    rust_time_per_call = timeit(lambda: fibbers.fib(n), number=RUNS) / RUNS
     print(f"\nRust μs per call: {rust_time_per_call * 1_000_000:.2f} μs")
     print(f"Rust ms per call: {rust_time_per_call * 1_000:.2f} ms")
 
