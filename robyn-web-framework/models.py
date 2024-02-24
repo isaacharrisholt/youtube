744716c3a404631c359fbd809f77943c12bcc623
Isaac Harris-Holt <isaac@harris-holt.com>
2024-02-24 15:18:02 +0000

    experiment with robyn

---
+++ robyn-web-framework/models.py
@@ -0,0 +1,22 @@
+from sqlalchem.orm import Boolean, Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+Base = declarative_base()
+
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    is_superuser = Column(Boolean, default=False)
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
