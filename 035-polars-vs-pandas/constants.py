1090959be5f25fcca1df8aa486a4b3a2cfd915d1
Isaac Harris-Holt <isaac@harris-holt.com>
2024-02-24 16:02:35 +0000

    035: correct constant

--- 035-polars-vs-pandas/constants.py
+++ 035-polars-vs-pandas/constants.py
@@ -1,4 +1,4 @@
-NYC_TAXI_TRIP_DATA = "./data/yellow_tripdata_2023-01.parquet"
+NYC_TAXI_TRIP_DATA = "./data/yellow_tripdata_2023-08.parquet"
 NYC_TAXI_TRIP_DATA_URL = "https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page"
 
 IMDB_REVIEWS_DATA = "./data/imdb_dataset.csv"
